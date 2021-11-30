fb651a120b57a1d5c1b33c90cbf2c642b5c0c301
unknown <leeviva@gmail.com>
2021-11-30 18:51:50 +0200

    react router exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import NotFound from './NotFound';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/About">About</Link>{' '}
+          <Link to="/Contact">Contact</Link>{' '}
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="*"    element={<NotFound/> } />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
